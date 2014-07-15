ec30c73c8fa175ebd594c5250d8aa8a9611ee587
dan <guevarra@caltech.edu>
2014-07-15 12:43:43 -0700

    orbPM function in pmconv converts JCAP-HTE platemap (ascii) to Orbis stage file binary. Platemap path and z-height are required arguments. Optionally, xoff and yoff are x- and y-offsets for platemap coords (+x right, +y up).

---
+++ pmconv.py
@@ -0,0 +1,49 @@
+#import os
+from readplatemap import readsingleplatemaptxt
+import struct, binascii
+# Read wafer platemap into dlist
+
+# os.chdir('/home/dan/code/orbisTools/')
+
+# pm='/media/work/data/platemaps/0027_flat2_y.txt'
+
+def orbPM(pm, zstg, xoff=0, yoff=0):
+    # assume pm in current working directory
+    dlist=readsingleplatemaptxt(pm)
+    
+    # assemble long string of bytes (as hex) then unhexlify and write to file
+    # form header string
+    header=struct.pack('<b',15)
+    header+=struct.pack('x')*15
+    header+=struct.pack('x')*2
+    header+=struct.pack('<h', dlist[-1]['Sample'])
+    header+=struct.pack('x')*20
+    header=header.encode('hex')
+    
+    # empty strings
+    index=''
+    positions=''
+    seperator='0000DD24664052B8884298AEC04285EBB140BE9F3A40486186422F1DC242C3F590400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    
+    for d in dlist:
+        i=struct.pack('<h', d['Sample'])# entry index (16-bit short?, 2 bytes)
+        i+=struct.pack('<h', 0) # entry type (00 for point, 01 for line, 02 for matrix)
+        i+=struct.pack('<h', 1)*2   # (1) num points to scan (01 for point, for line length, matrix width)
+                                    # (2) num points to scan (01 for point & line, matrix height)      
+        i=binascii.hexlify('Center                  ')+i.encode('hex')
+        x=struct.pack('<f', 100-d['x']+xoff)
+        y=struct.pack('<f', d['y']+yoff)
+        z=struct.pack('<f', zstg)
+        p=x+y+x+y+z+z
+        p+=struct.pack('x')*4 # 4 byte padding
+        p=p.encode('hex')
+        index+=i
+        positions+=p
+    
+    bytecode=binascii.unhexlify(header+index+seperator+positions)
+    
+    #os.chdir('/home/dan/code/orbisTools')
+    stgout=pm[:-3]+'stg'
+    f=open(stgout, mode='w')
+    f.write(bytecode)
+    f.close()
