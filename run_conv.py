52b83080868465a8878912fc20b64e280e3b0cab
Dan Guevarra <beatspace9@gmail.com>
2015-04-28 11:27:33 -0700

    Added list variable 'keepcodes' for omitting samples with codes not in list.

--- run_conv.py
+++ run_conv.py
@@ -1,16 +1,17 @@
 from pmconv import *
-pm='0049-04-0830-mp.txt'
-zstg=97.500
+pm='0049-04-0830-mp_code0-2-20.txt'
+keepcodes=[2]
+zstg=99.350
 xtweak=0
-a=1238
-ax=78.717
-ay=19.420
-b=236
-bx=76.616
-by=71.210
-c=457
-cx=13.568
-cy=60.860
+a=562
+ax=53.000
+ay=54.750
+b=7
+bx=53.070
+by=88.990
+c=582
+cx=12.330
+cy=54.710
 
 
-orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy)
+orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes)
