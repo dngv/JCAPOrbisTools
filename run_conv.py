adebd45235a43c9e963b791d52a516454211e5fe
Dan <beatspace9@gmail.com>
2015-07-13 09:26:22 -0700

    Updated run example for latest code changes.

--- run_conv.py
+++ run_conv.py
@@ -3,16 +3,16 @@ pm='0037-04-0730-mp.txt'
 keepcodes=[0, 2]
 pctmod=10
 zstg=97.500
-xtweak=0.2
+xtweak=0
 a=1808
-ax=83.920
-ay=27.420
+ax=83.770
+ay=25.990
 b=16
-bx=84.250
-by=84.380
-c=1836
-cx=27.010
-cy=27.360
+bx=83.530
+by=82.900
+c=1840
+cx=18.760
+cy=25.390
 
 
 orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes, pctmod)
