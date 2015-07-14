Encapsulated all arithmetic in numpy.float32 data type and "pctmod" feature for discrete platemaps will scale properly on thickness < 100%. Corrected PVD wafer rotation, but this still depends on wafer size and assumed platemap alignment on wafer (centered).

pmconv.py
@@ -1,9 +1,10 @@
 from readplatemap import readsingleplatemaptxt
-import struct, binascii, math
+import struct, binascii
+import numpy as np
 
 
 # generates stage file with same name as platemap, .stg extension
-def orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes=[0], pctmod=0, smpmod=0):
+def orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes=[], pctmod=0, smpmod=0):
 # xorg=100-xtweak #tweak the left edge origin (orbis motor can travel ~101mm +x)
     if not 0<=zstg<=100:
         print 'Invalid z-height'
@@ -25,62 +26,71 @@ def orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes=[0], pctm
         pby=dlist[bind]['y']
         pcx=dlist[cind]['x']
         pcy=dlist[cind]['y']
-        
-        # PM x & y diffs (origin sample A)
-        pabx=pbx-pax
-        paby=pby-pay
-        pab=(pabx**2+paby**2)**0.5
-        pacx=pcx-pax
-        pacy=pcy-pay
-        pac=(pacx**2+pacy**2)**0.5
-        
-        # Orbis x & y diffs (origin sample A)
-        sabx=ax-bx
-        saby=by-ay
-        sab=(sabx**2+saby**2)**0.5
-        sacx=ax-cx
-        sacy=cy-ay
-        sac=(sacx**2+sacy**2)**0.5
-        
-        rot=math.atan(sacy/sacx) # epson printer has non-linear elongation in y, use x instead
 
-        skx=sac/pac
-        sky=sab/pab
-        
+        # PM x & y diffs (origin sample A)
+        pabx=np.float32(pbx-pax)
+        paby=np.float32(pby-pay)
+        pab=np.float32((pabx**2+paby**2)**0.5)
+        pacx=np.float32(pcx-pax)
+        pacy=np.float32(pcy-pay)
+        pac=np.float32((pacx**2+pacy**2)**0.5)
+
+        # correct x-axis mirror
+        ax=np.float32(100-ax)
+        bx=np.float32(100-bx)
+        cx=np.float32(100-cx)
+
+        # Orbis x & y diffs (origin sample A)
+        sabx=np.float32(bx-ax)
+        saby=np.float32(by-ay)
+        sab=np.float32((sabx**2+saby**2)**0.5)
+        sacx=np.float32(cx-ax)
+        sacy=np.float32(cy-ay)
+        sac=np.float32((sacx**2+sacy**2)**0.5)
+
+        rot=np.arctan(sacy/sacx) # epson printer has non-linear elongation in y, use x instead
+
+        skx=np.float32(sac/pac)
+        sky=np.float32(sab/pab)
+
         print('x_skew = ' + "{:.3f}".format(skx) + ', y_skew = ' + "{:.3f}".format(sky) + ' , rot = ' + "{:.3f}".format(rot))
-			
+        	
         # empty strings
         index=''
         positions=''
-        
+
         # assemble long strings of bytes (as hex) then unhexlify and write to binary file                
         seperator='0000DD24664052B8884298AEC04285EBB140BE9F3A40486186422F1DC242C3F590400000'
         seperator=seperator+(struct.pack('x')*60).encode('hex')
-        
+
         counter=0
         
         for d in dlist:
-            if d['code'] not in keepcodes:
-              continue
-            if pctmod > 0 and sum([100*d[i]%pctmod for i in ['A', 'B', 'C', 'D']]) > 0:
-              continue
+            if len(keepcodes)!=0:
+                if d['code'] not in keepcodes:
+                    continue
+            compsum=np.sum([d[i] for i in ['A', 'B', 'C', 'D']])
+            if pctmod > 0 and compsum > 0:
+                if any([np.float32(100*d[i]/compsum)%pctmod > 0 for i in ['A', 'B', 'C', 'D']]):
+                    # print 'Skipped ' + str(d['Sample']) + ' ' + str([d[i] for i in ['A', 'B', 'C', 'D']])
+                    continue
             if smpmod > 0 and d['Sample']%smpmod > 0:
-              continue
-            xn=d['x']-pax # offset by new origin at sample A
-            yn=d['y']-pay
-            
-            xr=xn*math.cos(rot)-yn*math.sin(rot) # rotate first around sample A
-            yr=xn*math.sin(rot)+yn*math.cos(rot)
-            
-            xsk=xr*skx # skewed and rotated coord
-            ysk=yr*sky
-            
-            xstg=ax-xsk+xtweak # restore stage origin wrt sample A
-            ystg=ay+ysk
-            
+                continue
+            xn=np.float32(d['x']-pax) # offset by new origin at sample A
+            yn=np.float32(d['y']-pay)
+
+            xr=np.float32(xn*np.cos(rot)-yn*np.sin(rot)) # rotate first around sample A
+            yr=np.float32(xn*np.sin(rot)+yn*np.cos(rot))
+
+            xsk=np.float32(xr*skx) # skewed and rotated coord
+            ysk=np.float32(yr*sky)
+
+            xstg=np.float32(100-(ax+xsk+xtweak)) # restore stage origin wrt sample A
+            ystg=np.float32(ay+ysk)
+
             checkx=0<=xstg<=100
             checky=0<=ystg<=100
-            
+
             if checkx and checky:
                 counter+=1
                 i=struct.pack('<h', counter)# entry index (16-bit short?, 2 bytes), don't use sample number in case we remove out-of-range samples
@@ -97,7 +107,7 @@ def orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes=[0], pctm
                 p=p.encode('hex')
                 index+=i
                 positions+=p
-        
+
         # form header string, need to know # of in-range samples so this comes after for loop
         header=struct.pack('<b',15)
         header+=struct.pack('x')*15
@@ -105,10 +115,10 @@ def orbPM(pm, zstg, xtweak, a, ax, ay, b, bx, by, c, cx, cy, keepcodes=[0], pctm
         header+=struct.pack('<h', counter)
         header+=struct.pack('x')*20
         header=header.encode('hex')
-        
+
         # concatenate hex string and convert to byte code
         bytecode=binascii.unhexlify(header+index+seperator+positions)
-        
+
         #os.chdir('/home/dan/code/orbisTools')
         stgout=pm[:-3]+'stg'
         f=open(stgout, mode='wb') # writing binary

pvdconv.py
@@ -1,64 +1,78 @@
 from readplatemap import readsingleplatemaptxt
-import struct, binascii, math
+import struct, binascii
+import numpy as np
 
 
 # generates stage file with same name as platemap, .stg extension
-#def orbPM(pm, zstg, xoff=0, yoff=0):
-def orbPVD(pm, zstg, ax, ay, bx, by, cx, cy, keepcodes=[0], smpmod=0):
+def orbPVD(pm, zstg, ax, ay, bx, by, cx, cy, keepcodes=[], smpmod=0):
     if not 0<=zstg<=100:
         print 'Invalid z-height'
-        
+
     else:
         # assume pm in current working directory
         dlist=readsingleplatemaptxt(pm)
+
+        # ax, ay, bx, by, cx, cy : x-coord, y-coord (orbis convention +x left)
+        # a=point on left side of wafer flat, b=point on right side of wafer flat, c=leftmost point on wafer edge, cannot be on flat
+
+        # correct x-axis mirror
+        ax=np.float32(100-ax)
+        bx=np.float32(100-bx)
+        cx=np.float32(100-cx)
+
+        # Orbis x & y diffs (origin sample A)
+        sabx=np.float32(bx-ax)
+        saby=np.float32(by-ay)
+
+        # Si wafer diameter
+        dia=99.5
+        rad=np.float32(dia/2)
+        flat=2.73
+        pym=np.float32((dia-flat)/2)
+
+        rot=np.arctan(saby/sabx)
+
+        # stage rotation origin = (cx+rad, cy) -- because cy is leftmost point on wafer edge, should be at y=rad
+        rox=np.float32(cx+rad)
+        roy=np.float32(cy)
+
+        # y-position of rotated A-B line parallel to x-axis, this is platemap y=0
+        yoff=np.float32(roy+(ax-rox)*np.sin(-rot)+(ay-roy)*np.cos(-rot))
+        xoff=np.float32(cx)
         
-        #ax, ay, bx, by, cx, dy : x-coord, y-coord (orbis convention +x left)
-        #a=point on left side of wafer flat, b=point on right side of wafer flat, c=leftmost point on wafer edge, cannot be on flat
-        
-        #Orbis x & y diffs (origin sample A)
-        sbax=ax-bx
-        sbay=by-ay
-        
-        # Si wafer height/2
-        hh=47.3
-        # Si wafer width/2
-        hw=50
-        
-        rab=math.atan(sbay/sbax)
-        rot=rab
-        yoff=cy-hh
 
         print('rotation = ' + "{:.3f}".format(rot) + ', y-offset = ' + "{:.3f}".format(yoff))
-			
+        	
         # empty strings
         index=''
         positions=''
-        
+
         # assemble long strings of bytes (as hex) then unhexlify and write to binary file                
         seperator='0000DD24664052B8884298AEC04285EBB140BE9F3A40486186422F1DC242C3F590400000'
         seperator=seperator+(struct.pack('x')*60).encode('hex')
-        
-        counter=0
-        
-        for d in dlist:
-            if d['code'] not in keepcodes:
-              continue
-            if smpmod > 0 and d['Sample']%smpmod > 0:
-              continue
-            
-            # # rotate around pm center
-            xn=d['x']-hw
-            yn=d['y']-hh
-            xr=xn*math.cos(rot)-yn*math.sin(rot)+hw # rotate first
-            yr=xn*math.sin(rot)+yn*math.cos(rot)+hh
 
-            # # offset pm by point C coord
-            xstg=cx-xr
-            ystg=yoff+yr
-            
+        counter=0
+
+        for d in dlist:
+            if len(keepcodes)!=0:
+                if d['code'] not in keepcodes:
+                    continue
+            if smpmod > 0 and d['Sample']%smpmod > 0:
+                continue
+
+            # rotate around platemap center
+            xn=np.float32(d['x']-rad)
+            yn=np.float32(d['y']-pym)
+            xr=np.float32(xn*np.cos(rot)-yn*np.sin(rot)+rad)
+            yr=np.float32(xn*np.sin(rot)+yn*np.cos(rot)+pym)
+
+            # offset by rotated stage diffs
+            xstg=np.float32(100-xr+xoff)
+            ystg=np.float32(yr+yoff)
+
             checkx=0<=xstg<=100
             checky=0<=ystg<=100
-            
+
             if checkx and checky:
                 counter+=1
                 i=struct.pack('<h', counter)# entry index (16-bit short?, 2 bytes), don't use sample number in case we remove out-of-range samples
@@ -75,7 +89,7 @@ def orbPVD(pm, zstg, ax, ay, bx, by, cx, cy, keepcodes=[0], smpmod=0):
                 p=p.encode('hex')
                 index+=i
                 positions+=p
-        
+
         # form header string, need to know # of in-range samples so this comes after for loop
         header=struct.pack('<b',15)
         header+=struct.pack('x')*15
@@ -83,10 +97,10 @@ def orbPVD(pm, zstg, ax, ay, bx, by, cx, cy, keepcodes=[0], smpmod=0):
         header+=struct.pack('<h', counter)
         header+=struct.pack('x')*20
         header=header.encode('hex')
-        
+
         # concatenate hex string and convert to byte code
         bytecode=binascii.unhexlify(header+index+seperator+positions)
-        
+
         #os.chdir('/home/dan/code/orbisTools')
         stgout=pm[:-3]+'stg'
         f=open(stgout, mode='wb') # writing binary